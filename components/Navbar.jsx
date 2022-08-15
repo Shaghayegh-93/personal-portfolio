ee0827968475c2900265e40be09e3641df31dce0
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2022-08-15 23:54:21 +0430

    improve code base on ui of project

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,13 +31,55 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className="md:hidden">
-              <AiOutlineMenu />
+              <AiOutlineMenu size={25} />
             </div>
           </div>
         </div>
-      </div>
-      <div className="">
-
+        <div className=" fixed top-0 left-0 w-full h-screen bg-black/70">
+          <div className="fixed top-0 left-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-5 ease-in duration-500">
+            <div className="flex items-center justify-between w-full">
+              <div>
+                <Image
+                  src="/assets/logo.png"
+                  alt="logo"
+                  width="80"
+                  height="80"
+                />
+              </div>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's build something legendary together
+              </p>
+            </div>
+            <div className="py-4 flex flex-col
+            ">
+              <ul className="">
+                <Link href="/">
+                  <li className="py-4 text-sm">HOME</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">ABOUT</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">SKILLS</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">PROJECT</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">RESUME</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">CONTACT</li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
