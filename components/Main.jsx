b7f3240a2bf5ea2d9ad8ac018aaf0cab3d281677
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2022-08-19 23:14:40 +0430

    add Main component

---
+++ components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full felx items-center justify-between  p-40 mx-auto">
+        <div>
+          <p className="tracking-widest text-sm text-gray-600 ">
+            LET'S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4">
+            Hi, I'm <span className="text-[#64748b]"> Shaghayegh</span>
+            <br />
+            <span className="py-2">A Front-End Web Developer</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
